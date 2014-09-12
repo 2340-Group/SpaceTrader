fe99b9e33e25ebe76659a0d1fd861758a022b8a3
Diego Lewis <dlewis46@gatech.edu>
2014-09-11 23:36:14 -0400

    Start button takes you to configure screen

--- SpaceTrader/src/spacetrader/FXMLDocumentController.java
+++ SpaceTrader/src/spacetrader/FXMLDocumentController.java
@@ -12,6 +12,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import java.util.*;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.*;
+
 
 /**
  *
@@ -21,11 +28,19 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    private String currentScene;
+    
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void handleStartButtonAction(ActionEvent event) throws Exception {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        //label.setText("Hello World!");
+        currentScene = "configure";
+        Parent config = FXMLLoader.load(getClass().getResource("ConfigurationScreen.fxml"));
+        Scene sceneConfig = new Scene(config);
+        Stage stageN = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stageN.setScene(sceneConfig);
+        stageN.show();
     }
     
     @Override
