79af4da71a1a0d083f583de44da67d49ace6f1d2
Otoronata <kiahadjkhan@gmail.com>
2014-09-19 12:39:25 -0400

    added more methods to the economy class, make sure each SolarSystem object has an economy as an instance variable. Read my javadocs for more information

--- SpaceTrader/src/spacetrader/Economy.java
+++ SpaceTrader/src/spacetrader/Economy.java
@@ -6,18 +6,33 @@
 */
 public class Economy {
 
+    private Double currentInflation;
+    private final int ECON_SCORE_MAX = 10;
+    private final int ECON_SCORE_MIN = 0;
+    private ArrayList<Resource> resources;
+
+    /**
+    * Instantiates an instance of an Economy for a SolarSystem object.
+    * Stores the system's available resources in an ArrayList.
+    * initializes the economyScore as 5.
+    * initializes inflation as baseline.
+    */  
+    public Economy (ArrayList<Resource> r) {
+        resources = r;
+        economyScore = 5;
+        currentInflation = 1.0;
+    }
     /**
     * returns a Raised price by a percent multiplier
     * @param The Resource object having its price raised and the percent it's being raised by
     * @return the modified price of the Resource object
     * Example of invocation : Economy.rasiePrice(metal, 10);
     */
-    public static int raisePrice(Resource r, Double percent) {
+    public static void raisePrice(Resource r, Double percent) {
         percent = percent/100;
         int price = r.getPrice();
-        price = price + price*percent;
-        return price;
-
+        price = (price + price*percent) * currentInflation;
+        r.setPrice = price;
     }
 
     /**
@@ -26,7 +41,31 @@ public class Economy {
     * @return the modified price of the Resource object
     * Example of invocation : Economy.lowerPrice(metal, 10);
     */
-    public static int lowerPrice(Resource r, Double percent) {
-
+    public static void lowerPrice(Resource r, Double percent) {
+        percent = percent/100;
+        int price = r.getPrice();
+        price = (price - price*percent) * currentInflation;
+        r.setPrice = price;
     }
+
+    /**
+    * Checks the economy and prints out the inflation rate based on the system's economyScore.
+    * @return True if trading is possible, false if unable to trade.
+    */
+    public boolean checkEconomy() {
+        if (economyScore == ECON_SCORE_MIN) {
+            System.out.println("The economy has fallen apart in this system.");
+            System.out.println("No one can afford to trade here.");
+            return false;
+        }
+        else if (economyScore == ECON_SCORE_MAX) {
+            System.out.println("The economy is thriving in this system.");
+            System.out.println("No one has any interest to trade here.");
+            return false;
+        } else {
+            System.out.println("The current inflation rate is " + currentInflation + ".");
+            return true;
+        }
+    }
+
 }
