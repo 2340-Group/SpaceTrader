9b76113ac75d5893552db59af4568bbc84141372
JamieRule <jerule@gatech.edu>
2014-10-20 22:55:31 -0400

    The ShipUpgrade buttons all deduct the proper funds without giving the Equipment. The epitome of broken, but it is logically sound.

--- SpaceTrader/src/spacetrader/ShipUpgradeController.java
+++ SpaceTrader/src/spacetrader/ShipUpgradeController.java
@@ -1,5 +1,6 @@
 package spacetrader;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -13,10 +14,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class ShipUpgradeController implements Initializable{
 	
+    private Media deny;
+    private MediaPlayer mediaPlayer;
+    
 	@FXML
 	private Button weaponBasic;
 	@FXML
@@ -69,7 +75,40 @@ public class ShipUpgradeController implements Initializable{
 	 */
 	@FXML
     private void handleBuy(ActionEvent event) throws Exception {
-		// Not coding this until after lunch (currently 5:57pm)
+		Button clicked = (Button) event.getSource();
+		Equipment e = Equipment.NOTHING;
+		boolean isGadget = false;
+		if(clicked.equals(weaponBasic) || clicked.equals(sheildBasic)){
+			e = Equipment.BASIC_FUEL;
+		}else if(clicked.equals(weaponNormal) || clicked.equals(sheildNormal)){
+			e = Equipment.NORMAL_CARGO;
+		}else if(clicked.equals(weaponAdvanced) || clicked.equals(sheildAdvanced)){
+			e = Equipment.ADVANCED_ESC;
+		}else if(clicked.equals(gadgetFuel)){
+			e = Equipment.BASIC_FUEL;
+			isGadget = true;
+		}else if(clicked.equals(gadgetCargo)){
+			e = Equipment.NORMAL_CARGO;
+			isGadget = true;
+		}else if(clicked.equals(gadgetEscape)){
+			e = Equipment.ADVANCED_ESC;
+			isGadget = true;
+		}
+		if(e != Equipment.NOTHING)
+		{
+			int fund = MainController.getPlayer().getFunds();
+	   		if(fund >= e.getPrice())
+	   		{
+	   			fund = fund - e.getPrice();
+	   			MainController.getPlayer().setFunds(fund);
+	   			funds.setText("" + fund);
+                
+		   		greyOut();
+	   		}else{
+	   			mediaPlayer = new MediaPlayer(deny);
+	            mediaPlayer.play();
+	   		}
+		}
 	}
 	
     /**
@@ -96,6 +135,8 @@ public class ShipUpgradeController implements Initializable{
 	@Override
 	public void initialize(URL location, ResourceBundle resources) 
 	{
+		deny = new Media(new File("./src/spacetrader/Glitch Smashvox 2.wav").toURI().toString());
+        mediaPlayer = new MediaPlayer(deny);
 		funds.setText("" + MainController.getPlayer().getFunds());
 		priceBasicW.setText("" + Equipment.BASIC_FUEL.getPrice());
 		priceNormalW.setText("" + Equipment.NORMAL_CARGO.getPrice());
@@ -106,27 +147,6 @@ public class ShipUpgradeController implements Initializable{
 		priceBasicS.setText("" + Equipment.BASIC_FUEL.getPrice());
 		priceNormalS.setText("" + Equipment.NORMAL_CARGO.getPrice());
 		priceAdvanceS.setText("" + Equipment.ADVANCED_ESC.getPrice());
-		Ship ship = MainController.getPlayer().getShip();
-		ShipType st = ship.getType();
-		int have = ship.getWeapons().size();
-		int canHave = st.getWeaponSlots();
-		maxWeapon.setText("Own: " + have + "\nof possible " + canHave);
-		if(have == canHave)
-		{
-			weaponBasic.setDisable(true);
-			weaponNormal.setDisable(true);
-			weaponAdvanced.setDisable(true);
-		}
-		
-		have = ship.getSheilds().size();
-		canHave = st.getSheildSlots();
-		maxSheild.setText("Own: " + have + "\nof possible " + canHave);
-		if(have == canHave)
-		{
-			sheildBasic.setDisable(true);
-			sheildNormal.setDisable(true);
-			sheildAdvanced.setDisable(true);
-		}
 		
 		int tech = MainController.getPlanetNotString().getTechLevel();
 		switch(tech)	// fall through on purpose
@@ -152,6 +172,33 @@ public class ShipUpgradeController implements Initializable{
 	   	default:
 	   		break;	//tech level seven has all ships
 	   	}
+		
+		greyOut();
+	}
+	
+	private void greyOut()
+	{
+		Ship ship = MainController.getPlayer().getShip();
+		ShipType st = ship.getType();
+		int have = ship.getWeapons().size();
+		int canHave = st.getWeaponSlots();
+		maxWeapon.setText("Own: " + have + "\nof possible " + canHave);
+		if(have == canHave)
+		{
+			weaponBasic.setDisable(true);
+			weaponNormal.setDisable(true);
+			weaponAdvanced.setDisable(true);
+		}
+		
+		have = ship.getSheilds().size();
+		canHave = st.getSheildSlots();
+		maxSheild.setText("Own: " + have + "\nof possible " + canHave);
+		if(have == canHave)
+		{
+			sheildBasic.setDisable(true);
+			sheildNormal.setDisable(true);
+			sheildAdvanced.setDisable(true);
+		}
 		
 		have = ship.getGadgets().size();
 		canHave = st.getGadgetSlots();
