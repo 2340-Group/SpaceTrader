6abca9fc94ea8d86a33a248a38a2f5d14148fb82
JamieRule <jerule@gatech.edu>
2014-10-28 17:46:46 -0400

    .

--- SpaceTrader/src/spacetrader/ShipTest_jrule3.java
+++ SpaceTrader/src/spacetrader/ShipTest_jrule3.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class jrule3_ShipTest {
+public class ShipTest_jrule3 {
 	Ship defaultShip;
 	Ship mostGadgets;
 	Ship noGadgets;
