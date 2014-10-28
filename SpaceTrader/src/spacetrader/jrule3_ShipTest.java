327ac84a9d25750d14a1e4612d7162252075f02f
JamieRule <jerule@gatech.edu>
2014-10-28 17:33:05 -0400

    Added JUnit Library started JUnit class for getGadget Ship method

---
+++ SpaceTrader/src/spacetrader/jrule3_ShipTest.java
@@ -0,0 +1,29 @@
+package spacetrader;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class jrule3_ShipTest {
+	Ship defaultShip;
+	Ship mostGadgets;
+	Ship noGadgets;
+	Ship oneGadget;
+	
+	@Before
+	public void createShips()
+	{
+		defaultShip = new Ship();
+		mostGadgets = new Ship(ShipType.GRASSHOPPER, 0, " ", null, null, null, 0, 0);
+		noGadgets = new Ship(ShipType.FLEA, 0, " ", null, null, null, 0, 0);
+		oneGadget = new Ship(ShipType.HORNET, 0, null, null, null, null, 0, 0);
+	}
+	
+	@Test
+	public void testAddSingleGadget() {
+		defaultShip.addGadget(Equipment.BASIC);
+		assertTrue(defaultShip.getGadgets().contains(Equipment.BASIC));
+	}
+
+}
