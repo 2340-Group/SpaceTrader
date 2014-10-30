068127cd51d3e402482a68a93e2031b28d2bb0e4
JamieRule <jerule@gatech.edu>
2014-10-30 15:50:23 -0400

    JUnit begin

--- SpaceTrader/src/spacetrader/ShipTest_jrule3.java
+++ SpaceTrader/src/spacetrader/ShipTest_jrule3.java
@@ -5,25 +5,50 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Tests the single method addGadget() in Ship.java
+ * @author Jamie
+ */
 public class ShipTest_jrule3 {
+    /**
+     * noGadgets - has no gadget slots
+     * oneGadget - has one gadget slot
+     * mostGadgetsThree - has three gadgets slots, the most any ship has
+     * defaultShip - the ship that Player starts with at the beginning of the game
+     */
     Ship defaultShip;
-    Ship mostGadgets;
+    Ship mostGadgetsThree;
     Ship noGadgets;
     Ship oneGadget;
-
+    
+    /**
+     * creates the ships as specified by their names
+     */
     @Before
     public void createShips() {
 	defaultShip = new Ship();
-	mostGadgets = new Ship(ShipType.GRASSHOPPER, 0, " ", null, null, null,
+	mostGadgetsThree = new Ship(ShipType.GRASSHOPPER, 0, " ", null, null, null,
 		0, 0);
 	noGadgets = new Ship(ShipType.FLEA, 0, " ", null, null, null, 0, 0);
 	oneGadget = new Ship(ShipType.HORNET, 0, null, null, null, null, 0, 0);
     }
-
+    
+    /**
+     * Ship should not be able to take gadgets
+     */
+    @Test
+    public void testAddGadgetWhenNoSlots(){
+	assertFalse(noGadgets.addGadget(Equipment.BASIC));
+	assertTrue(noGadgets.getGadgets().isEmpty());
+    }
+    
+    /**
+     * basic adding one gadget
+     * simplest test, no exceptions or extraordinary circumstances
+     */
     @Test
     public void testAddSingleGadget() {
-	defaultShip.addGadget(Equipment.BASIC);
+	assertTrue(defaultShip.addGadget(Equipment.BASIC));
 	assertTrue(defaultShip.getGadgets().contains(Equipment.BASIC));
     }
-
 }
