e2f3ee8c46513af673f4b3a32f3e36825f64d673
JamieRule <jerule@gatech.edu>
2014-11-10 19:24:06 -0500

    Gradle

---
+++ SpaceTrader/src/main/java/GradleMain.java
@@ -0,0 +1,15 @@
+package main.java;
+
+import spacetrader.*;
+
+/**
+ * gradle build calls this class which will call the rest of SpaceTrader
+ * 
+ * @author Jamie
+ */
+public class GradleMain {
+
+    public static void main(String[] args) {
+	SpaceTrader.main(args);
+    }
+}
